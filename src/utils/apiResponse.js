a62874d09d7c52ee0c69e0f195ff586048bbae2b
TusharChakraborty07 <chakrabortytushar684@gmail.com>
2026-05-25 22:34:23 +0530

    added

--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -1,11 +1,10 @@
 class ApiResponse {
-  constructor(statusCode, message, data = null, error = null) {
-    this.success = statusCode < 400;
-    this.statusCode = statusCode;
-    this.message = message;
-    this.data = data;
-    this.error = error;
-  }
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
 }
 
-module.exports = ApiResponse;
+export { ApiResponse }
